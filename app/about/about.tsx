78acff569598a668444d2f304a60050b549f41ca
Jacob Chippett <jchippet@uoguelph.ca>
2025-07-28 16:12:22 -0400

    added dummy about page

---
+++ app/about/about.tsx
@@ -0,0 +1,11 @@
+export function About() {
+    return (
+		<main className="flex items-center justify-center pt-16 pb-4">
+			<div className="flex-1 flex flex-col items-center gap-16 min-h-0">
+				<p className="leading-6 text-gray-700 dark:text-gray-200 text-center">
+					about us :D
+				</p>
+			</div>
+		</main>
+	);
+}
