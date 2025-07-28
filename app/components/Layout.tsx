7048e0cc5e6b5ff1a53405395fb04da22c12c57f
ahasrati <hasratiamirhossein@gmail.com>
2025-07-28 18:12:59 -0400

    Created very basic Layout and EventCard components.

---
+++ app/components/Layout.tsx
@@ -0,0 +1,13 @@
+import type React from "react";
+import { Outlet } from "react-router";
+
+const Layout: React.FC = () => {
+	return (
+		<div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
+			{/** Navbar will go here */}
+			<Outlet />
+		</div>
+	);
+};
+
+export default Layout;
