9127a1415bbc3f01584611320bc89d533234000e
ahasrati <hasratiamirhossein@gmail.com>
2025-08-10 00:23:28 -0400

    Update about page

--- app/components/AboutPage.tsx
+++ app/components/AboutPage.tsx
@@ -2,34 +2,55 @@ export function About() {
 	return (
 		<main className="flex items-center justify-center pt-16 pb-4">
 			<div className="flex-1 flex flex-col text-xl gap-16 min-h-0">
-				<h1 className="bungee my-32 text-center text-2xl sm:text-4xl md:text-5xl lg:text-6xl">
+				<h1 className="bungee my-16 text-center text-2xl sm:text-4xl md:text-5xl lg:text-6xl">
 					All Players Welcome — No Skill Tree Required
 				</h1>
-				<p>
-					Welcome to the official hub for gamers at the University of Guelph —
-					where players, creators, and fans come together to share their passion
-					for all things gaming. Whether you're into casual party games,
-					competitive esports, or exploring virtual worlds, our club has a place
-					for you.
-				</p>
-				<p>
-					We host a variety of events throughout the year, including online
-					tournaments, in-person game nights, and community scrims. It’s a space
-					to connect, compete, and just have fun with fellow students who love
-					gaming as much as you do.
-				</p>
-				<p>
-					No matter your skill level or favorite genre, our goal is to build an
-					inclusive and energetic gaming community on campus.
-				</p>
-				<div className="grid grid-cols-2 gap-6">
+				<div className="flex w-full flex-col items-center gap-16 p-4 md:flex-row">
+					<p className="w-full md:w-1/2">
+						Welcome to the official hub for gamers at the University of Guelph —
+						where players, creators, and fans come together to share their
+						passion for all things gaming. Whether you're into casual party
+						games, competitive esports, or exploring virtual worlds, our club
+						has a place for you.
+					</p>
+					<img
+						src="/images/gg1.jpg"
+						alt="A video game controller."
+						className="w-full md:w-1/2 h-auto object-cover rounded-md"
+					></img>
+				</div>
+				<div className="flex w-full flex-col items-center gap-16 p-4 md:flex-row">
+					<img
+						src="/images/gg2.jpg"
+						alt="Students playing video games at a club event."
+						className="w=full md:w-1/2 h-auto object-cover rounded-md"
+					></img>
+					<p className="w-full md:w-1/2">
+						We host a variety of events throughout the year, including online
+						tournaments, in-person game nights, and community scrims. It’s a
+						space to connect, compete, and just have fun with fellow students
+						who love gaming as much as you do.
+					</p>
+				</div>
+				<div className="flex w-full flex-col items-center gap-16 p-4 md:flex-row">
+					<p className="w-full md:w-1/2">
+						No matter your skill level or favorite genre, our goal is to build
+						an inclusive and energetic gaming community on campus.
+					</p>
+					<img
+						src="/images/gg3.JPG"
+						alt=""
+						className="w=full md:w-1/2 h-auto object-cover rounded-md"
+					></img>
+				</div>
+				{/* <div className="grid grid-cols-2 gap-6">
 					<img className="rounded-2xl" src="/images/FightNight.JPG" alt="wingstop fight night"></img>
 					<p>very epic and cool wingstop at fight night!</p>
 					<p>mario maddness very sugoi!</p>
 					<img className="rounded-2xl" src="/images/MarioMadness.JPG" alt="mario madness"></img>
 					<img className="rounded-2xl" src="/images/LAN2023.jpg" alt="LAN event"></img>
 					<p>epic amazing LAN event!</p>
-				</div>
+				</div> */}
 			</div>
 		</main>
 	);
