4a16e83e2e70fac8b8ada49f65f760957e7b5a95
Jacob Chippett <jchippet@uoguelph.ca>
2025-08-25 15:14:09 -0400

    added updateEvent() function

--- app/lib/supabase/dataAccess.ts
+++ app/lib/supabase/dataAccess.ts
@@ -28,7 +28,7 @@ const fetchEvent = async (id: string): Promise<Event> => {
 	return data as Event;
 };
 
-//given an Event as a parameter the function insterts the Event into the database
+//given an Event as a parameter the function inserts the Event into the database
 const createEvent = async ( evnt: Event): Promise<void> => {
 	const { error } = await supabase
 		.from("events")
@@ -48,4 +48,27 @@ const createEvent = async ( evnt: Event): Promise<void> => {
 	}
 };
 
-export { fetchAllEvents, fetchEvent, createEvent };
+//given an Event as a parameter the function updates an existing event in the database
+const updateEvent = async ( evnt: Event): Promise<void> => {
+	const { error } = await supabase
+		.from("events")
+		.update({ 
+			id: evnt.id, 
+			title: evnt.title, 
+			description: evnt.description,
+			date: evnt.date,
+			location: evnt.location,
+			created_at: evnt.created_at,
+			updated_at: evnt.updated_at,
+			created_by: evnt.created_by
+		})
+		.eq("id", evnt.id);
+
+	if (error) {
+		throw error;
+	}
+};
+
+
+
+export { fetchAllEvents, fetchEvent, createEvent, updateEvent };
